b5131de5368970af95650023ab134d5760f44f69
natashaslprice <natashaslprice@gmail.com>
2015-12-11 10:06:28 -0800

    ready for first deployment

--- public/controllers.js
+++ public/controllers.js
@@ -35,6 +35,7 @@ angular.module('myApp.controllers', [])
     }; // end of searchForm function
   }]) // end of LandingIndexCtrl
 
+
   .controller('SongsIndexCtrl', ['$scope', '$http','$window', '$element', '$location', 'sharedAlbums', function ($scope, $http, $window, $element, $location, sharedAlbums) { 
     // get albums from sharedAlbums factory
     $scope.albums = sharedAlbums.getAlbums();
@@ -57,9 +58,10 @@ angular.module('myApp.controllers', [])
     }; // end of getTracks function
 
     // find if bronze silver or gold level selected
+    $scope.value = 'Bronze';
     $scope.newValue = function(value) {
-      $scope.value = value;
-      console.log($scope.value);
+      $scope.level = value;
+      console.log($scope.level);
     };
 
     // get lyrics for chosen track
@@ -76,68 +78,114 @@ angular.module('myApp.controllers', [])
           // console.log("here", $scope.trackUri);
 
           // make lyrics into array, using spaces and line breaks
-          console.log(response);
+          // console.log(response);
           var responseArray = response.replace( /\n/g, " " ).split( " " );
-          // console.log(responseArray);
+          // console.log(responseArray.length);
 
-          // get no.of words based on that and length of response array
-          // set array of indexes empty
-          // for loop until array length is equal to the length of no. of words
-          // choose random number between 1 and length of response array
-          // for loop through indexes array to check number doesn't already exist
-          // if array[that number] index of does not have an ' in, 
-            // then replace word with input, with data-id same as word
-            // and push that number into number array
+          // get no.of words to blank out based on level and length of response array
+          var numberOfBlanks;
+          // if gold, 30% of words blank
+          if ($scope.level == 'Gold') {
+            numberOfBlanks = Math.floor(0.3 * responseArray.length);
+          }
+          // if silver, 20% of words blank
+          else if ($scope.level == 'Silver') {
+            numberOfBlanks = Math.floor(0.2 * responseArray.length);
+          }
+          // if bronze, 10% of words blank
+          else {
+            numberOfBlanks = Math.floor(0.1 * responseArray.length);
+          }
+          console.log(numberOfBlanks);
 
-
-
-
-          // get 5 random numbers to use as indexes of words to remove from array
-          var wordIndexes = [];
-          while (wordIndexes.length < 5) {
-            var randomNumber = Math.floor((Math.random() * 50) + 1);
-            var found = false;
-            for (var j = 0; j < wordIndexes.length; j ++) {
-              if (randomNumber === wordIndexes[j]) {
-                found = true;
-                return;
+          // set array of indexes to empty
+          var blanksIndexes = [];
+          // while loop until blanksIndexes length is equal to the numberofBlanks
+          while (blanksIndexes.length !== numberOfBlanks) {
+            // choose random number between 1 and length of response array
+            var randomNumber = Math.floor((Math.random() * (responseArray.length)) + 1);
+            // if blanksIndexes empty, push randomNumber in
+            if (blanksIndexes.length === 0) {
+              blanksIndexes.push(randomNumber);
+              console.log(blanksIndexes);
+            }
+            else {
+              // for loop through blanksIndexes to check number doesn't already exist
+              var found = false;
+              for (var i = 0; i < blanksIndexes.length; i ++) {
+                if (randomNumber === blanksIndexes[i]) {
+                  found = true;
+                  return;
+                }
+              }
+              // if not found
+              if (!found) {
+                // if responseArray word does not have an ' in
+                for (var j = 0; j < blanksIndexes.length; j++) {
+                  if (responseArray[blanksIndexes[j]].indexOf('\'') < 0) {
+                    // then replace word with input, with data-id same as word
+                    var inputField = "<input type='text' class='lyricsInputs' data-id='" + responseArray[j] + "'>";
+                    console.log(inputField);
+                    // responseArray[j] = responseArray[j].replace(responseArray[j], inputField);
+                    // // and push that number into number array
+                    // blanksIndexes.push(randomNumber);
+                  }
+                  else {
+                    return;
+                  }
+                }
               }
             }
-            if (!found) {
-              wordIndexes.push(randomNumber);
-            }
           }
-          wordIndexes.sort(function(a, b){
-            return a-b;
-          });
-          // console.log(wordIndexes);
+          console.log(blanksIndexes);
+          console.log(responseArray);
+    
+          // // get 5 random numbers to use as indexes of words to remove from array
+          // var wordIndexes = [];
+          // while (wordIndexes.length < 5) {
+          //   var randomNumber = Math.floor((Math.random() * 50) + 1);
+          //   var found = false;
+          //   for (var j = 0; j < wordIndexes.length; j ++) {
+          //     if (randomNumber === wordIndexes[j]) {
+          //       found = true;
+          //       return;
+          //     }
+          //   }
+          //   if (!found) {
+          //     wordIndexes.push(randomNumber);
+          //   }
+          // }
+          // wordIndexes.sort(function(a, b){
+          //   return a-b;
+          // });
+          // // console.log(wordIndexes);
 
 
 
           
-          // get the 5 words with the wordIndexes from the newResponseArray
-          var wordsToRemove = [];
-          var index;
-          for (var k = 0; k < wordIndexes.length; k++) {
-            index = wordIndexes[k];
-            // console.log(index);
-            // push the words into an array to store
-            wordsToRemove.push(newResponseArray[index]);
-            // replace the word with inputField
-            var inputField = "<input type='text' class='lyricsInputs' data-id='" + wordsToRemove[k] + "'>";
-            newResponseArray[index] = newResponseArray[index].replace(newResponseArray[index], inputField);
-          }
-          // console.log(wordsToRemove);
-          // console.log(newResponseArray);
+          // // get the 5 words with the wordIndexes from the newResponseArray
+          // var wordsToRemove = [];
+          // var index;
+          // for (var k = 0; k < wordIndexes.length; k++) {
+          //   index = wordIndexes[k];
+          //   // console.log(index);
+          //   // push the words into an array to store
+          //   wordsToRemove.push(newResponseArray[index]);
+          //   // replace the word with inputField
+          //   var inputField = "<input type='text' class='lyricsInputs' data-id='" + wordsToRemove[k] + "'>";
+          //   newResponseArray[index] = newResponseArray[index].replace(newResponseArray[index], inputField);
+          // }
+          // // console.log(wordsToRemove);
+          // // console.log(newResponseArray);
           
-          // make array a string again
-          var stringLyrics = newResponseArray.join(" ");
-          console.log(stringLyrics);
+          // // make array a string again
+          // var stringLyrics = newResponseArray.join(" ");
+          // console.log(stringLyrics);
           
-          // replace the line breaks with <br>
-          // var stringLyricsBreak = stringLyrics.replace(/\n/g, '<br>'); 
-          $('#lyrics').html(stringLyrics);
-          // $scope.lyrics = stringLyrics;
+          // // replace the line breaks with <br>
+          // // var stringLyricsBreak = stringLyrics.replace(/\n/g, '<br>'); 
+          // $('#lyrics').html(stringLyrics);
+          // // $scope.lyrics = stringLyrics;
 
 
         })
@@ -148,13 +196,6 @@ angular.module('myApp.controllers', [])
 
     
   }]); // end of SongsIndexCtrl
-
-
-        
-          
-
-      
-// array[i].indexOf('\'') >= 0
 
 
 
